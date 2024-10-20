d98ef24735f0c9d339394ee1988a6066bee3f6bf
Andre <Andrewlol774@gmail.com>
2024-10-20 11:01:10 -0600

    Added the main.py and also I added the json

---
+++ main.py
@@ -0,0 +1,24 @@
+# Primero juntar variables simbolos terminales 
+
+#   "N −→ cat | dog",
+#     "N −→ beer | cake | juice | meat | soup",
+
+
+# a   "N −→ cat | dog | beer | cake | juice | meat | soup"
+
+
+#Luego hacer cfg a cnf entonces
+#Agregar S0 -> S  lo denotamos como $ como S0 porque no
+
+#revisar que sean dos variables y revisar si ningun terminal (minusculas)  tienen variables
+
+#eliminar producciones unarias osea el S0 tiene que tener todo lo que tiene S
+# "S0 -> NP VP"
+
+#Eliminar si no llegan a ningun lado
+ 
+#  Ya tenemos chomskys
+
+
+#Implementar cyk
+# https://www.geeksforgeeks.org/cocke-younger-kasami-cyk-algorithm/
